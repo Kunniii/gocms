b8932a1f1df1ef928aa97ccad4d04ba8852c98c5
Kunniii <nguyenairport56@gmail.com>
2023-12-19 21:57:43 +0700

    .

--- internal/types/roles.go
+++ internal/types/roles.go
@@ -9,8 +9,8 @@ type Role struct {
 // Keep it in this order
 var Roles = []Role{
 	{ID: 0, Name: "user", Detail: "Normal User, which can comment"},
-	{ID: 1, Name: "admin", Detail: "The one who can do pretty much anything!"},
-	{ID: 2, Name: "contributor", Detail: "The one who can create post!"},
-	{ID: 3, Name: "publisher", Detail: "The one who can create and publish posts"},
-	{ID: 4, Name: "manager", Detail: "The one who can manage posts, tags...."},
+	{ID: 1, Name: "contributor", Detail: "The one who can create post!"},
+	{ID: 2, Name: "publisher", Detail: "The one who can create and publish posts"},
+	{ID: 3, Name: "manager", Detail: "The one who can manage posts, tags...."},
+	{ID: 4, Name: "admin", Detail: "The one who can do pretty much anything!"},
 }
--- internal/types/userClaims.go
+++ internal/types/userClaims.go
@@ -1,6 +1,9 @@
 package itypes
 
+import "github.com/golang-jwt/jwt/v5"
+
 type UserClaims struct {
+	jwt.Claims
 	UserID   uint
 	RoleID   uint
 	Email    string
